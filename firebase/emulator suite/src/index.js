2d3ecbeccb53144788c10f5e3bdb54516f09964a
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2023-01-02 20:20:59 +0500

    firebase & enumlator suite

---
+++ firebase/emulator suite/src/index.js
@@ -0,0 +1,9 @@
+/*
+ java 11+ required https://www.oracle.com/java/technologies/downloads/#jdk19-windows
+ - npm i firebase-tools -g
+ - firebase login
+ - firebase init  emulators
+ - auth: 9099, store: 8080, UI: 4000
+ - firebase emulators:start
+*/
+
